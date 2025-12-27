84647ddfa63230e36bb2d301363b57ddcafabcad
Hamada <hamadauter@gmail.com>
2025-12-28 00:09:54 +0300

    Removed placedholder text and added conditional logic for each button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Wrapper from "./components/Wrapper"
+import Screen from "./components/Screen"
+import ButtonBox from "./components/ButtonBox"
+import Button from "./components/Button"
+
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ".", "="],
+  ]
+
+  const [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res}/>
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button 
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={
+                  btn === "C"
+                  ? resetClickHandler
+                  : btn === "+-"
+                  ? invertClickHandler
+                  : btn === "%"
+                  ? percentClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
+                }
+              />
+            )
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   )
 }
 
